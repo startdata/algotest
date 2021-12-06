bd72cad3dc2fe4e2b780fd0e4c4f70c61cc27516
startas <83121954+startdata@users.noreply.github.com>
2021-12-06 18:34:24 +0900

    baekjoon 2839 fix

--- src/Main.java
+++ src/Main.java
@@ -7,33 +7,18 @@ public class Main {
 
         int N = in.nextInt();
 
-//        if (N == 4 || N == 7){
-//            System.out.println(-1);
-//        }
-//        else if (N % 5 == 0){
-//            System.out.println(N /5);
-//        }
-//        else if (N % 5 == 1 || N % 5 == 3){
-//            System.out.println((N / 5) + 1);
-//        }
-//        else if (N % 5 == 2 || N % 5 ==4){
-//            System.out.println((N/5) + 2);
-//        }
-
-        int A = N/5;
-        int C = N%5;
-
-        if (C == 0){
-            System.out.println(A);
+        if (N == 4 || N == 7){
+            System.out.println(-1);
         }
-        else if (C != 0){
-            int e = C/3;
-            if(e == 0){
-                System.out.println(A +1);
-            }
-            else if(e != 3){
-                System.out.println(-1);
-            }
+        else if (N % 5 == 0){
+            System.out.println(N /5);
         }
+        else if (N % 5 == 1 || N % 5 == 3){
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 ==4){
+            System.out.println((N/5) + 2);
+        }
+
     }
 }
