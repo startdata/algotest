f81b01d57aebd97d980f66d5745954a4f73db070
startas <83121954+startdata@users.noreply.github.com>
2021-12-07 19:12:34 +0900

    baekjoon 1076

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,35 @@
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int N = in.nextInt();
+        HashMap<String, String> colerMap = new HashMap<String,String>();
+        colerMap.put("black", "0,1");
+        colerMap.put("brown", "1,10");
+        colerMap.put("red", "2,100");
+        colerMap.put("orange", "3,1000");
+        colerMap.put("yellow", "4,10000");
+        colerMap.put("green", "5,100000");
+        colerMap.put("blue", "6,1000000");
+        colerMap.put("violet", "7,10000000");
+        colerMap.put("grey", "8,100000000");
+        colerMap.put("white", "9,1000000000");
 
-        if (N == 4 || N == 7){
-            System.out.println(-1);
-        }
-        else if (N % 5 == 0){
-            System.out.println(N /5);
-        }
-        else if (N % 5 == 1 || N % 5 == 3){
-            System.out.println((N / 5) + 1);
-        }
-        else if (N % 5 == 2 || N % 5 ==4){
-            System.out.println((N/5) + 2);
-        }
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();
+        sc.close();
+
+        String[] get1 = colerMap.get(a).split(",");
+        String[] get2 = colerMap.get(b).split(",");
+        String[] get3 = colerMap.get(c).split(",");
+
+        Long result = Long.parseLong(get1[0]+get2[0]) * Long.parseLong(get3[1]);
+        System.out.println(result);
 
     }
 }
